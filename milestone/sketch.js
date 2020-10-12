6d1396d7e1aa4bd5b6b729078decd6dc107e5aeb
s3661656 <68724251+s3661656@users.noreply.github.com>
2020-10-12 21:32:37 +1100

    Create sketch.js

---
+++ milestone/sketch.js
@@ -0,0 +1,37 @@
+var font;
+var vehicles = [];
+
+function preload(){
+  font = loadFont('DIN.otf')
+}
+function setup() {
+  createCanvas(800, 400);
+  background(51);
+ // textFont(font);
+  //textSize(192);
+  //fill(255);
+  //noStroke();
+  //text('COVID',100,200);
+  
+  var points = font.textToPoints('COVID',100,200,190);
+  console.log(points);
+  
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x,pt.y);
+    vehicles.push(vehicle); 
+    //stroke(255);
+    //strokeWeight(8);
+    //point(pt.x, pt.y);
+  }
+}
+
+function draw() {
+  background(51);
+  for (var i = 0; i < vehicles.length;i++){
+       var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+       }
+}
